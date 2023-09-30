1e99797d6f02c8bc4e29bdcb26833b5cd5b6073b
nerdthejohn <newtonjohn043@gmail.com>
2023-09-30 20:34:10 +0100

    chore: config

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
 import logging
 import time
+from kafka import KafkaProducer
 
 
-logger = logging.getLogger('my_logger')
+kafka_topic = "logs"
+kafka_producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
+logger = logging.getLogger('__name__')
+logger.setLevel(logging.DEBUG)
+
+
+class KafkaHandler(logging.Handler):
+    def emit(self, record):
+        log_message = self.format(record)
+       
+        kafka_producer.send(kafka_topic, value=log_message.encode('utf-8'))
+        kafka_producer.flush()
+        print(log_message)
+
+
+kafka_handler = KafkaHandler()
+kafka_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+kafka_handler.setFormatter(formatter)
+
+logger.addHandler(kafka_handler)
 
 while True:
     logger.info('This is a log message sent to Kafka.')
