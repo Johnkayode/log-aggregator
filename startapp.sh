d7d22e916c027b99eb356718eb93621fa3a56537
nerdthejohn <newtonjohn043@gmail.com>
2023-10-01 22:14:17 +0100

    feat: app wait for topic to be created

--- startapp.sh
+++ startapp.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
-# Function to check if the Kafka topic exists
-topic_exists() {
-    kafka-topics --zookeeper zookeeper:2181 --list | grep -q "logs"
+# Set your Kafka topic name
+KAFKA_TOPIC="logs"
+
+# Set your Kafka broker address
+KAFKA_BROKER="kafka:9092"
+
+# Function to check if a Kafka topic exists using Python
+check_kafka_topic() {
+    python - <<EOF
+from kafka.admin import KafkaAdminClient
+
+def topic_exists(broker, topic):
+    admin_client = KafkaAdminClient(bootstrap_servers=broker)
+    return topic in admin_client.list_topics()
+
+if topic_exists("$KAFKA_BROKER", "$KAFKA_TOPIC"):
+    exit(0)
+else:
+    exit(1)
+EOF
 }
 
-# Wait for the Kafka topic to exist (retry every second)
-while ! topic_exists; do
-    echo "Waiting for Kafka topic 'logs' to be created..."
-    sleep 1
-done
-
-# Start the Kafka producer
-python -u app.py
+# Check if the Kafka topic exists
+if check_kafka_topic; then
+    echo "Starting the application."
+    # Run your Python application here
+    python -u app.py
+else
+    echo "Waiting for Kafka topic '$KAFKA_TOPIC'."
+fi
